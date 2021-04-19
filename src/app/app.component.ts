fd3d60aafb3e993aa317856e26a69e17376a1ef5
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-04-19 17:27:50 -0300

    ✨ feat: color-picker logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-chrome-extension';
+  color: string;
+
+  public updateColor(color: string) {
+    chrome.storage.sync.set({ color });
+  }
+
+  ngOnInit(): void {
+    chrome.storage.sync.get('color', ({ color }) => {
+      this.color = color;
+    });
+  }
+
+  public colorize() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const id = tabs[0].id;
+      if (id == null) return;
+      chrome.tabs.executeScript(id, {
+        code: 'document.body.style.backgroundColor = "' + this.color + '";',
+      });
+    });
+  }
 }
