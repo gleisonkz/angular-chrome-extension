58895c804a1f98376df213d1e942af43366c3931
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-04-19 17:26:38 -0300

    ✨ feat: background script

---
+++ src/background.ts
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color: '#3aa757' });
+
+  chrome.webNavigation.onCompleted.addListener(
+    () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+        if (id == null) return;
+        chrome.pageAction.show(id);
+      });
+    },
+    { url: [{ urlMatches: 'google.com' }] }
+  );
+});
